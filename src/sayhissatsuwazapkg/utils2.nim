e63f7b2545382328dc67d58057a473c1dc207142
jiro <jiroron666@gmail.com>
2021-06-05 22:19:24 +0900

    chore: en_USの実装をja_JPと同じにした #24 (#26)
    * 共通化
    
    * format
    
    * 使わないプロシージャを削除
    
    * join string
    
    * format

---
+++ src/sayhissatsuwazapkg/utils2.nim
@@ -0,0 +1,21 @@
+import types2
+import sequtils
+
+func combine*(part: PartOfSpeech, texts: seq[string], attrs: seq[
+    Attribute]): seq[Word] =
+  for text in texts:
+    result.add Word(partOfSpeech: part, text: text, attrs: attrs)
+
+func addAttr*(words: seq[Word], attr: Attribute): seq[Word] =
+  for word in words:
+    var w = word
+    w.attrs.add attr
+    result.add w
+
+proc search*(words: seq[Word], cond: SearchCondition): seq[Word] =
+  result = words
+  # 品詞で絞り込み
+  result = result.filterIt(it.partOfSpeech == cond.partOfSpeech)
+  # conditionの属性すべてを満たすwordに絞り込みj
+  for attr in cond.attrs:
+    result = result.filterIt(attr in it.attrs)
