b402e24c18beabc5e0f9ab6d16f3a7f1435d3964
jiro <jiroron666@gmail.com>
2021-06-05 20:07:58 +0900

    feat: 日本語の必殺技の生成方法を変更 (#23)
    * テストコードを追加
    
    * フォーマット
    
    * WIP
    
    * Format
    
    * wip
    
    * wip
    
    * format
    
    * wip
    
    * wip
    
    * Revert "wip"
    
    This reverts commit a579a73e301326f5d5fd63d92af9ef4926992c86.
    
    * Revert "Format"
    
    This reverts commit 89717496c11b05f3369378353be2ea76e1074f71.
    
    * Revert
    
    * fix
    
    * fix test
    
    * fix cond

--- src/sayhissatsuwazapkg/lang/lang_ja_JP.nim
+++ src/sayhissatsuwazapkg/lang/lang_ja_JP.nim
@@ -1,56 +1,103 @@
-import tables, random
+import random, sequtils
 from unicode import toRunes
 
-import ../types
+type
+  Attribute = enum
+    kanji, katakana,
+    element, non, fire, ice, wind, thunder, holy, darkness,
+    attack, slash, blow, thrust,
+    assist
+
+  PartOfSpeech {.pure.} = enum
+    ## 品詞。
+    verb, # 動詞
+    noun  # 名詞
+
+  Word = object
+    partOfSpeech: PartOfSpeech
+    attrs: seq[Attribute]
+    text: string
+
+  SearchCondition = object
+    partOfSpeech: PartOfSpeech
+    attrs: seq[Attribute]
+
+func atk(text: string, attrs: seq[Attribute]): Word =
+  var a = @[attack]
+  a.add attrs
+  result = Word(partOfSpeech: noun, attrs: a, text: text)
+
+func atks(texts: seq[string], attrs: seq[Attribute]): seq[Word] =
+  for text in texts:
+    result.add atk(text, attrs)
+
+func elem(text: string, attrs: seq[Attribute]): Word =
+  var a = @[element]
+  a.add attrs
+  result = Word(partOfSpeech: noun, attrs: a, text: text)
+
+func elems(texts: seq[string], attrs: seq[Attribute]): seq[Word] =
+  for text in texts:
+    result.add elem(text, attrs)
+
+func assistWord(text: string, attrs: seq[Attribute]): Word =
+  var a = @[assist]
+  a.add attrs
+  result = Word(partOfSpeech: noun, attrs: a, text: text)
+
+func assistWords(texts: seq[string], attrs: seq[Attribute]): seq[Word] =
+  for text in texts:
+    result.add assistWord(text, attrs)
+
+func cond(p: PartOfSpeech, a: seq[Attribute]): SearchCondition =
+  SearchCondition(partOfSpeech: p, attrs: a)
 
 const
-  generators = @[
-    Generator(lang: ja, syllabary: kanji, pattern: @[attrFire, attrSlash]),
-    Generator(lang: ja, syllabary: kanji, pattern: @[attrWind, attrThunder,
-        attrBlow]),
-    Generator(lang: ja, syllabary: kanji, pattern: @[attrHoly, attrThrust]),
-    Generator(lang: ja, syllabary: kanji, pattern: @[attrDarkness, attrSlash]),
-    Generator(lang: ja, syllabary: kanji, pattern: @[attrNon, attrFire,
-        attrBlow]),
-    Generator(lang: ja, syllabary: katakana, pattern: @[attrFire, attrSlash]),
-    Generator(lang: ja, syllabary: katakana, pattern: @[attrHoly, attrThrust]),
-    Generator(lang: ja, syllabary: katakana, pattern: @[attrNon, attrBlow]),
-    Generator(lang: ja, syllabary: katakana, pattern: @[attrIce, attrWind]),
+  words = concat(
+    elems(@["火炎", "紅蓮", "炎", "爆炎", "炎帝", "黒炎"], @[fire,
+        kanji]),
+    elems(@["氷結", "氷"], @[ice, kanji]),
+    elems(@["風神", "疾風", "旋風", "風"], @[wind, kanji]),
+    elems(@["雷", "雷神", "雷光", "雷鳴", "電光"], @[thunder, kanji]),
+    elems(@["聖", "光"], @[holy, kanji]),
+    elems(@["闇", "暗黒"], @[darkness, kanji]),
+
+    assistWords(@["業", "連", "裂"], @[kanji]),
+
+    elems(@["ファイア", "フレイム"], @[fire, katakana]),
+    elems(@["アイス", "アイシクル", "フリーズ"], @[ice, katakana]),
+    elems(@["ウィンド", "ストーム"], @[wind, katakana]),
+    elems(@["サンダー", "プラズマ"], @[thunder, katakana]),
+    elems(@["セイント", "ホーリー", "ライト"], @[holy, katakana]),
+    elems(@["ダーク", "ダークネス"], @[darkness, katakana]),
+
+    atks(@["斬", "剣", "刃"], @[slash, kanji]),
+    atks(@["撃", "掌"], @[blow, kanji]),
+    atks(@["突"], @[thrust, kanji]),
+
+    atks(@["スラッシュ", "ブレード", "ソード"], @[slash, katakana]),
+    atks(@["ブロウ", "クラッシュ", "アタック", "ブレイク"], @[
+        blow, katakana]),
+    atks(@["スラスト", "ピアース"], @[thrust, katakana]),
+
+  )
+
+  patterns = @[
+    @[cond(noun, @[kanji, element]), cond(noun, @[kanji, attack])],
+    @[cond(noun, @[katakana, element]), cond(noun, @[katakana, attack])],
+    @[cond(noun, @[kanji, assist]), cond(noun, @[kanji, element]), cond(noun, @[
+        kanji, attack])],
+    @[cond(noun, @[kanji, wind]), cond(noun, @[kanji, thunder]), cond(noun, @[
+        kanji, attack])],
   ]
 
-  elementWords = {
-    kanji: {
-      non: @["強", "業", "連", "列"],
-      fire: @["火炎", "紅蓮", "炎", "爆炎", "炎帝", "黒炎"],
-      ice: @["氷結", "氷"],
-      wind: @["風神", "疾風", "旋風", "風"],
-      thunder: @["雷", "雷神", "雷光"],
-      holy: @["聖", "光"],
-      darkness: @["闇", "暗黒"],
-    }.toTable,
-    katakana: {
-      non: @["ブレイブ", "エクスプロージョン", "ファイナル"],
-      fire: @["ファイア", "フレイム"],
-      ice: @["アイス", "アイシクル", "フリーズ"],
-      wind: @["ウィンド", "ストーム"],
-      thunder: @["サンダー", "プラズマ"],
-      holy: @["セイント", "ホーリー", "ライト"],
-      darkness: @["ダーク", "ダークネス"],
-    }.toTable,
-  }.toTable
-
-  attackWords = {
-    kanji: {
-      slash: @["斬", "剣", "刃"],
-      blow: @["撃", "掌"],
-      thrust: @["突"],
-    }.toTable,
-    katakana: {
-      slash: @["スラッシュ", "ブレード", "ソード"],
-      blow: @["ブロウ", "クラッシュ", "アタック", "ブレイク"],
-      thrust: @["スラスト", "ピアース"],
-    }.toTable,
-  }.toTable
+proc search(words: seq[Word], cond: SearchCondition): seq[Word] =
+  result = words
+  # 品詞で絞り込み
+  result = result.filterIt(it.partOfSpeech == cond.partOfSpeech)
+  # conditionの属性すべてを満たすwordに絞り込みj
+  for attr in cond.attrs:
+    result = result.filterIt(attr in it.attrs)
 
 proc generate*(): string =
   ## 日本語の必殺技名をランダムに生成する。
@@ -60,16 +107,22 @@ proc generate*(): string =
     randomize()
     echo generate()
 
-  let gen = generators.sample
-  for i, attr in gen.pattern:
-    case attr.kind
-    of element:
-      result.add elementWords[gen.syllabary][attr.fElement].sample
-    of attack:
-      # 最後のattributeの時は文字数調整で奇数個になるようにする。
-      # attackはいずれも1文字の漢字なので、奇数個の時は1文字の装飾とセットで結合して5文字になるようにする
-      if gen.pattern.len == i+1 and
-          gen.syllabary == kanji and
-          result.toRunes.len mod 2 == 1:
-        result.add elementWords[gen.syllabary][non].sample
-      result.add attackWords[gen.syllabary][attr.fAttack].sample
+  let pattern = patterns.sample
+  for i, cond in pattern:
+    let word = words.search(cond).sample
+    # 最後のattributeの時は文字数調整で奇数個になるようにする。
+    # attackはいずれも1文字の漢字なので、奇数個の時は1文字の装飾とセットで結合して5文字になるようにする
+    if pattern.len == i+1 and
+        kanji in cond.attrs and
+        result.toRunes.len mod 2 == 1:
+      let matched = words
+        .search(SearchCondition(partOfSpeech: noun, attrs: @[kanji, assist]))
+        .filterIt(it.text.toRunes[0] notin result.toRunes)
+      if 0 < matched.len:
+        result.add matched.sample.text
+    result.add word.text
+
+when isMainModule:
+  randomize()
+  for i in 1..1000:
+    echo generate()
